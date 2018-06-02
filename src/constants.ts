b3e566a9e2d91853a8a19b633f02e8a6ee19ecd8
Paul Koerbitz <paul.koerbitz@gmail.com>
2018-06-02 18:21:19 +0200

    Generalize type parameter names and handle non-intrinsic types

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const FILENAME = '__ts-type-test-inline-e1d70ff1__';
+export const FILENAME_RE = new RegExp(FILENAME);
+export const TYPENAME_RE = /type\s+(__[a-zA-Z0-9][_a-zA-Z0-9]*)[^_a-zA-Z0-9]+/g;
+
--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,97 @@
-#!/usr/bin/env ts-node
 import ts from 'typescript';
-const FILENAME = "__ts-type-test-inline-e1d70ff1__"
-const FILENAME_RE = new RegExp(FILENAME);
+import { FILENAME, FILENAME_RE, TYPENAME_RE } from './constants';
 
 /**
  * TODOS:
- * - generalize parameter names & positions
- * - correctly extract compilation options
- * - make npm library out of this
- * - comment this code
- * - allow injecting options
+ * - [x] generalize parameter names & positions
+ * - [x] handle non-intrinsic types correctly
+ * - [ ] correctly extract compilation options
+ * - [ ] comment this code
+ * - [ ] make npm library out of this
+ * - [ ] allow injecting options
  */
 
-const resolveTypes = (code: string) => {
-    let sf: ts.SourceFile;
-    const ch = ts.createCompilerHost({})
-    const getSourceFile = (fileName: string, languageVersion: ts.ScriptTarget, ...args: any[]) => {
+export const resolveTypes = (code: string, options?: ts.CompilerOptions) => {
+    const result: { [key: string]: string } = {};
+    const finalOptions = resolveOptions(options);
+    const compilerHost = ts.createCompilerHost(finalOptions);
+    let inlineCode: ts.SourceFile;
+    const getSourceFile = (
+        fileName: string,
+        languageVersion: ts.ScriptTarget,
+        ...args: any[]
+    ) => {
         if (!FILENAME_RE.test(fileName)) {
-            return (ch.getSourceFile as any)(fileName, languageVersion, ...args);
+            return (compilerHost.getSourceFile as any)(
+                fileName,
+                languageVersion,
+                ...args
+            );
         }
-        if (sf == undefined) {
-            sf = ts.createSourceFile(FILENAME, code, languageVersion);
+        if (inlineCode == undefined) {
+            inlineCode = ts.createSourceFile(FILENAME, code, languageVersion);
         }
-        return sf;
+        return inlineCode;
     };
-    const myCh = {
-        ...ch,
+    const customCompilerHost = {
+        ...compilerHost,
         getSourceFile
     };
-    const prog = ts.createProgram([FILENAME], {}, myCh);
-    const tc = prog.getTypeChecker();
-    console.log(sf!.statements.map(stmt => [stmt, extract(tc.getTypeAtLocation(stmt))]));
-    // console.log();
-    // const type = tc.getTypeAtLocation(sf.statements[2]);
-    // // console.log(flags, JSON.stringify(symbols.map(s => s.getName()).length, undefined, 4));
-    // // const filtered = symbols.filter(symbol => /__1/.test(symbol.getName())).map(symbol =>
-    // //     tc.getTypeOfSymbolAtLocation(symbol, sf)
-    // // );
-    // return type;
+    const program = ts.createProgram(
+        [FILENAME],
+        finalOptions,
+        customCompilerHost
+    );
+
+    const isTypeNameRegexp = new RegExp(extractTypeNames(code).join('|'));
+    const checker = program.getTypeChecker();
+    checker
+        .getSymbolsInScope(inlineCode!.endOfFileToken, ts.SymbolFlags.Type)
+        .filter(symbol => isTypeNameRegexp.test(symbol.name))
+        .map(symbol => ({
+            type: checker.getDeclaredTypeOfSymbol(symbol),
+            name: symbol.name
+        }))
+        .forEach(({ type, name }) => {
+            result[name] = checker.typeToString(
+                type,
+                inlineCode,
+                ts.TypeFormatFlags.InTypeAlias
+            );
+        });
+    return result;
 };
 
-const extract = (x: any) => ({
-    flags: x.flags,
-    id: x.id,
-    intrinsicName: x.intrinsicName
-});
-
-const onStringLit = (s: TemplateStringsArray) => {
-    return resolveTypes(s[0]);
+export const resolveTypesOnTemplate = (
+    s: TemplateStringsArray,
+    ...args: any[]
+) => {
+    let input: string[] = [];
+    for (let i = 0; i < s.length; ++i) {
+        if (i > 0) {
+            input.push(`__${args[i - 1]}`);
+        }
+        input.push(s[i]);
+    }
+    return resolveTypes(input.join(''));
 };
 
-onStringLit`
-type __1 = string;
-`;
+/**
+ * Extract the type names from the inline code
+ * e.g. for `type __1 = string;` return ['__1'];
+ *
+ * @param code The inline source code
+ * @returns an array of type names
+ */
+const extractTypeNames = (code: string) => {
+    let names: string[] = [];
+    let match: RegExpExecArray | null = null;
+    while ((match = TYPENAME_RE.exec(code))) {
+        names.push(match[1]);
+    }
+    return names;
+};
+
+// FIXME: load projects compiler options if none given
+const resolveOptions = (options?: ts.CompilerOptions): ts.CompilerOptions =>
+    options || {};
