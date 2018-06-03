11c13512f6fe14029db871caf1f29eb18c118527
Paul Koerbitz <paul.koerbitz@gmail.com>
2018-06-03 18:08:03 +0200

    Create README with todo list

---
+++ README.md
@@ -0,0 +1,13 @@
+TODOS:
+- [x] generalize parameter names & positions
+- [x] handle non-intrinsic types correctly
+- [x] correctly extract compilation options
+- [x] allow injecting options
+- [ ] comment this code
+- [ ] write readme
+- [ ] tests
+- [ ] Automate prettification, test running, etc
+- [ ] make npm library out of this
+- [ ] blog post
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,2 @@
-
-/**
- * TODOS:
- * - [x] generalize parameter names & positions
- * - [x] handle non-intrinsic types correctly
- * - [ ] correctly extract compilation options
- * - [ ] allow injecting options
- * - [ ] comment this code
- * - [ ] tests
- * - [ ] make npm library out of this
- * - [ ] blog post
- */
-
 export { setOptions } from './options';
 export { resolveTypes } from './resolve-types';
-
