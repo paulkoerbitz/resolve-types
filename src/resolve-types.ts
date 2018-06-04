41b2c26f25c8fb4e4c8f18ac133427742dd38edf
Paul Koerbitz <paul.koerbitz@gmail.com>
2018-06-04 15:20:10 +0200

    Refactor and comment functions in resolve-types

--- src/resolve-types.ts
+++ src/resolve-types.ts
@@ -2,43 +2,52 @@ import * as ts from 'typescript';
 import { FILENAME, FILENAME_RE, TYPENAME_RE } from './constants';
 import { getOptions } from './options';
 
-export const resolveTypes = (input: string | TemplateStringsArray, ...args: any[]) => {
-    const code = createInlineCode(input, args);
+/**
+ * Resolve type declarations in inline code.
+ *
+ * resolveTypes can be invoked with a string or template literal.
+ * The types which should be resolved must be prefixed with two
+ * underscores (e.g. __myType). When invoked with a template
+ * literal interpolated entries will automatically be prefixed
+ * with two underscores.
+ *
+ * Returns a map from declared type names to the string representation
+ * of the type they evaluate to.
+ *
+ * Examples:
+ *
+ * resolveTypes("type __1 = string;") returns { __1: "string" }
+ *
+ * resolveTypes`
+ *    type ${0} = number;
+ *    type ${1} = string;
+ * `
+ * returns { __0: "number", __1: "string" }
+ *
+ * resolveTypes`
+ *    type ${0} = Pick<{ a: number; b: string; c: any }, "a" | "b">;
+ * `
+ * returns { __0: "{ a: number; b: string; }" }
+ */
+export const resolveTypes = (
+    input: string | TemplateStringsArray,
+    ...args: any[]
+) => {
     const result: { [key: string]: string } = {};
-    const options = getOptions();
-    const compilerHost = ts.createCompilerHost(options);
-    let inlineCode: ts.SourceFile;
-    const getSourceFile = (
-        fileName: string,
-        languageVersion: ts.ScriptTarget,
-        ...args: any[]
-    ) => {
-        if (!FILENAME_RE.test(fileName)) {
-            return (compilerHost.getSourceFile as any)(
-                fileName,
-                languageVersion,
-                ...args
-            );
-        }
-        if (inlineCode == undefined) {
-            inlineCode = ts.createSourceFile(FILENAME, code, languageVersion);
-        }
-        return inlineCode;
-    };
-    const customCompilerHost = {
-        ...compilerHost,
-        getSourceFile
-    };
-    const program = ts.createProgram(
-        [FILENAME],
-        options,
-        customCompilerHost
-    );
+
+    const code = createInlineCode(input, args);
+    const { program, inlineSourceFile } = createInlineProgram(code);
 
     const isTypeNameRegexp = new RegExp(extractTypeNames(code).join('|'));
     const checker = program.getTypeChecker();
+
+    // Find our special type names, get their types, print them and put them
+    // into the result object
     checker
-        .getSymbolsInScope(inlineCode!.endOfFileToken, ts.SymbolFlags.Type)
+        .getSymbolsInScope(
+            inlineSourceFile!.endOfFileToken,
+            ts.SymbolFlags.Type
+        )
         .filter(symbol => isTypeNameRegexp.test(symbol.name))
         .map(symbol => ({
             type: checker.getDeclaredTypeOfSymbol(symbol),
@@ -47,18 +56,22 @@ export const resolveTypes = (input: string | TemplateStringsArray, ...args: any[
         .forEach(({ type, name }) => {
             result[name] = checker.typeToString(
                 type,
-                inlineCode,
+                inlineSourceFile,
+                // We need this TypeFormatFlags to avoid getting
+                // the type alias we created back
                 ts.TypeFormatFlags.InTypeAlias
             );
         });
+
     return result;
 };
 
-const createInlineCode = (
-    s: TemplateStringsArray | string,
-    args: any[]
-) => {
-    if (typeof s === "string") {
+/**
+ * Convert a template literal to appropriately formatted
+ * inline code or use the inline code directly
+ */
+const createInlineCode = (s: TemplateStringsArray | string, args: any[]) => {
+    if (typeof s === 'string') {
         return s;
     }
     let input: string[] = [];
@@ -85,4 +98,47 @@ const extractTypeNames = (code: string) => {
         names.push(match[1]);
     }
     return names;
+};
+
+/**
+ * Use the inline code to create a TypeScript program
+ * To do so, we provide a fake file name and a custom
+ * compile host which returns the inline code as source
+ * file
+ *
+ * @param code The inline code we want to turn into a program
+ */
+const createInlineProgram = (code: string) => {
+    // Work around definite assignemt checking: inlineSourceFile is assigned
+    // when ts.createProgram is created
+    let inlineSourceFile!: ts.SourceFile;
+    const getSourceFile = (
+        fileName: string,
+        languageVersion: ts.ScriptTarget,
+        ...args: any[]
+    ) => {
+        if (!FILENAME_RE.test(fileName)) {
+            return (compilerHost.getSourceFile as any)(
+                fileName,
+                languageVersion,
+                ...args
+            );
+        }
+        if (inlineSourceFile == undefined) {
+            inlineSourceFile = ts.createSourceFile(
+                FILENAME,
+                code,
+                languageVersion
+            );
+        }
+        return inlineSourceFile;
+    };
+    const options = getOptions();
+    const compilerHost = ts.createCompilerHost(options);
+    const customCompilerHost = {
+        ...compilerHost,
+        getSourceFile
+    };
+    const program = ts.createProgram([FILENAME], options, customCompilerHost);
+    return { program, inlineSourceFile };
 };
