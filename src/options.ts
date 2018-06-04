817e364d9caf7250c25463dba0c93881d9d1c350
Paul Koerbitz <paul.koerbitz@gmail.com>
2018-06-04 23:50:41 +0200

    Comment options.ts

--- src/options.ts
+++ src/options.ts
@@ -1,8 +1,25 @@
 import * as fs from 'fs';
 import * as ts from 'typescript';
 
+/**
+ * The global options object which setOptions and getOptions work on
+ */
 let options: ts.CompilerOptions;
 
+/**
+ * Set compiler options for checking inline code
+ *
+ * These options set a global options object which any subsequent
+ * invocations of `resolveTypes` will use.
+ *
+ * ignoreProjectOptions controls if the options from tsconfig are
+ * used or not.
+ *
+ * @param input Compiler options for use when checking inline code.
+ * @param ignoreProjectOptions If true, do not merge the passed options with
+ *     the project options (from tsconfig.json) but only use the passed options
+ *     exclusively. False by default.
+ */
 export const setOptions = (input: ts.CompilerOptions, ignoreProjectOptions = false): void => {
     if (ignoreProjectOptions) {
         options = input;
@@ -20,6 +37,9 @@ export const setOptions = (input: ts.CompilerOptions, ignoreProjectOptions = fal
     options = { ...config, ...input };
 };
 
+/**
+ * Get the TypeScript compiler options to be used for resolving types
+ */
 export const getOptions = (): ts.CompilerOptions => {
     if (options == undefined) {
         setOptions({});
